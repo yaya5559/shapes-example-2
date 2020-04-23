2ab4b3dd3382363bba3242ab9f1e713aa7adfff0
Carlos R. Arias <arias@spu.edu>
2020-04-23 16:27:04 -0700

    working on main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,53 @@
-
+#include "shape.h"
 #include "triangle.h"
+#include "isosceles.h"
+#include "ellipse.h"
+#include "circle.h"
+#include "rectangle.h"
+#include "square.h"
 
 #include <iostream>
+#include <vector>
 using std::cout;
+using std::cin;
+using std::cerr;
 using std::endl;
+using std::vector;
 
-void PrintTriangleData(const Triangle& triangle);
+size_t Menu();
 
 int main() {
-	Triangle t1(4,3,5);
-	PrintTriangleData(t1);
-
-	Triangle t2(1,2,2);
-	PrintTriangleData(t2);
-
+	vector<Shape*> shapes;
 	return 0;
 }
-void PrintTriangleData(const Triangle& triangle){
-	cout << triangle.ToString() << endl;
-	cout << triangle.Area() << endl;
-	cout << triangle.Perimeter() << endl;
-	cout << triangle.AngleAB() << "\t" << triangle.AngleBC() << "\t" << triangle.AngleAC() << endl;
-	cout << endl;
+
+size_t Menu(){
+	int option = 0;
+	while(true){
+		cout << "1. Add an Ellipse" << endl;
+		cout << "2. Add a Circle" << endl;
+		cout << "3. Add a Triangle" << endl;
+		cout << "4. Add an Isosceles Triangle" << endl;
+		cout << "5. Add a Rectangle" << endl;
+		cout << "6. Add a Square" << endl;
+		cout << "7. List all Ellipses" << endl;
+		cout << "8. List all Triangles" << endl;
+		cout << "9. List all Rectangles" << endl;
+		cout << "10. List all Shapes" << endl;
+		cout << "11. Exit" << endl;
+
+		cin >> option;
+		if (cin.fail()){
+			cin.clear();
+			cin.ignore(255, '\n');
+			cerr << "Incorrect input!" << endl;
+			continue;
+		}
+		if (option < 1 || option > 11){
+			cerr << "Incorrect menu option!" << endl;
+			continue;
+		}
+		break;
+	}
+	return static_cast<size_t>(option);
 }
